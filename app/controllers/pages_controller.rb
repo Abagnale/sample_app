08235372bdb06a99fd93f2fd23bdb736435d8a68
Adrien Saumureau <adrien.saumureau@gmail.com>
2013-04-11 10:14:55 +0100

    end of chapter 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+	@titre = "home"
   end
 
   def contact
+	@titre = "contact"
   end
   
   def about
+	@titre = "about"
   end
   
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,13 +9,27 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have a good title" do
+      get 'home'
+      response.should have_selector("title", :content => 
+					"Simple App du Tutoriel Ruby on Rails | home")
+    end
+    
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
+		get 'contact'
       response.should be_success
     end
+    
+    it "should  have good title" do
+		get 'contact'
+		response.should have_selector( "title", :content =>
+					"Simple App du Tutoriel Ruby on Rails | contact")
+    end
+    
   end
   
   describe "GET 'about'" do
@@ -23,6 +37,13 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    
+	it "should  have good title" do
+		get 'about'
+		response.should have_selector( "title", :content =>
+					"Simple App du Tutoriel Ruby on Rails | about")
+    end
+    
   end
 
 end
