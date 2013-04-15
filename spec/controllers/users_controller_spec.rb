79bf8de87c7dbc1a89adfef965db7e54487e6ce5
Adrien Saumureau <adrien.saumureau@gmail.com>
2013-04-15 13:04:50 +0100

    end of chapter 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,12 +10,12 @@ describe UsersController do
 
 	describe "GET 'new'" do
 		it "returns http success" do
-			get 'new'
+			get :new
 			response.should be_success
 		end
 
 		it "should have good title" do
-		get 'new'
+		get :new
 		response.should have_selector("title", :content =>
 						@base_title + "Inscription")
 		end
@@ -51,6 +51,59 @@ describe UsersController do
     it "devrait avoir une image de profil" do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+  
+  
+  describe "POST 'create'" do
+
+    describe "echec" do
+
+      before(:each) do
+        @attr = { :nom => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "ne devrait pas creer d'utilisateur" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "devrait avoir le bon titre" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Inscription")
+      end
+
+      it "devrait rendre la page 'new'" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "succes" do
+
+      before(:each) do
+        @attr = { :nom => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "devrait creer un utilisateur" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "devrait rediriger vers la page d'affichage de l'utilisateur" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end   
+      
+      it "devrait avoir un message de bienvenue" do
+        post :create, :user => @attr
+        flash[:success].should =~ /Bienvenue dans l'Application Exemple/i
+      end
+       
     end
   end
 
